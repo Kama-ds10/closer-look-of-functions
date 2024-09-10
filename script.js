066962800def968c50891a16bafa38a657702fc6
kama_ds10 <kamazd1234@gmail.com>
2024-09-10 03:49:01 +0100

    how passing arguments works values vs reference

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 // default parameters
-
+/*
 const bookings = [];
 
 const createBooking = function (flightNum, numPassengers = 1, price = 199) {
@@ -18,3 +18,37 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 
 createBooking('LM123');
 createBooking('lMH123', 2, 5);
+*/
+// how passing arguments works values vs reference
+
+const flight = 'LM234';
+const Kama = {
+  name: 'Kama Seleye-fubara',
+  passport: 283893994,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'Lm2566';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 283893994) {
+    alert('checked in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+// checkIn(flight, Kama);
+// console.log(flight);
+// console.log(Kama);
+
+// is the same as doing..
+// const flightNum = flight;
+// const passenger = Kama;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() + 10000000000);
+};
+
+newPassport(Kama);
+checkIn(flight, Kama);
